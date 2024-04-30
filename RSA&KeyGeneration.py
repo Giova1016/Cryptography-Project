bfe9ab34e6255f16f596442e1c42b1d9a82d4dc0
Giovanni Roman Fernandez <gio.rom.fdz@gmail.com>
2024-04-30 13:00:19 -0400

    Made some slight indentation fixes to RSA&KeyGeneration.py

--- RSA&KeyGeneration.py
+++ RSA&KeyGeneration.py
@@ -112,20 +112,25 @@ def is_prime(n, k=10):
     Returns:
     - Returns if the number generated is a prime (True) or if it is not a prime (False) after being tested.
     """
-    if n < 2: return False
+    if n < 2: 
+        return False
     for p in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]:
-        if n % p == 0: return n == p
+        if n % p == 0: 
+            return n == p
     s, d = 0, n - 1
     while d % 2 == 0:
         s, d = s + 1, d >> 1
     for i in range(k):
         x = random.randrange(2, n - 1)
         x = pow(x, d, n)
-        if x == 1 or x == n - 1: continue
+        if x == 1 or x == n - 1: 
+            continue
         for r in range(1, s):
             x = pow(x, 2, n)
-            if x == n - 1: break
-        else: return False
+            if x == n - 1: 
+                break
+        else: 
+            return False
     return True
 
 def rsa_encrypt(plaintext, public_key):
