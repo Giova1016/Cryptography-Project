cf43fad6859cff26ffd112f9bfddb1fc8903d68d
Giovanni Roman Fernandez <gio.rom.fdz@gmail.com>
2024-05-03 21:23:39 -0400

    Finished implementing the ECDH algorithm

--- DiffieHellmanKE_EllipticCurves.py
+++ DiffieHellmanKE_EllipticCurves.py
@@ -1,68 +1,5 @@
 import random 
-from PrimalityTests import miller_rabin_primality_test
-from affine_cipher import mod_inverse
-
-def is_point_on_curve(P, a, b, p):
-    """
-    Checks if the given point P lies on the elliptic curve y^2 = x^3 + a*x + b mod p.
-
-    Args:
-    - P: the point (x, y) to be checked
-    - a, b: coefficients of the elliptic curve equation
-    - p: the prime modulus
-
-    Returns:
-    - True if the point lies on the curve, False otherwise
-    """
-    x, y = P
-    
-    left = y ** 2
-    right = (x ** 3 + a * x + b) % p
-    
-    return left == right
-
-def is_curve_valid(a, b, p):
-    """
-    Checks if the given elliptic curve parameters a, b, and p are valid.
-
-    Args:
-    - a, b: coefficients of the elliptic curve equation
-    - p: the prime modulus
-
-    Returns:
-    - True if the elliptic curve parameters are valid, False otherwise
-    """
-    # checks if p is prime
-    if not miller_rabin_primality_test(p):
-        return False
-    if (4 * a ** 3 + 27 * b ** 2) % p == 0:
-        return False
-    
-    return True
-
-def double_and_add(P, n, a, b, p):
-    """
-    Perform point multiplication on an elliptic curve using the Double-and-Add Algorithm.
-
-    Parameters:
-    - P: the base point (x, y) on the elliptic curve
-    - n: the scalar to be multiplied with the base point
-    - a, b: coefficients of the elliptic curve equation
-    - p: the prime modulus
-
-    Returns:
-    - The resutl of the point multiplication (x, y)
-    """
-    Q = (0, 0)
-    R = P
-    
-    for bit in bin(n)[2:]:
-        if bit == '1':
-            Q = point_addition(Q, R, a, b, p)
-        R = point_doubling(R, a, b, p)
-        
-    return Q
-
+   
 def point_addition(P, Q, a, b, p):
     """
     Perform point addition on an elliptic curve. 
@@ -74,7 +11,7 @@ def point_addition(P, Q, a, b, p):
 
     Returns:
     - The result of the point addition (x, y)
-    """
+    """ 
     if P == (0, 0):
         return Q
     if Q == (0, 0):
@@ -82,46 +19,70 @@ def point_addition(P, Q, a, b, p):
     
     x1, y1 = P
     x2, y2 = Q
-    
+     
     if x1 == x2 and y1 == y2:
-        return point_doubling(P, a, b, p)
-    if x1 == x2:
-        return (0, 0)
-    
-    m = ((y2 - y1) * mod_inverse(x2 - x1, p)) % p
-    x3 = (m ** 2 - x1 - x2) & p
-    y3 = (m * (x1 - x3) - y1) % P
-    
-    return (x3, y3)
+        beta = (3*x1*x2 + a) * pow(2*y1, -1, p)
+    else:
+        beta = (y2 - y1) * pow(x2 - x1, -1, p)
+     
+    x3 = (beta*beta - x1 - x2) % p
+    y3 = (beta * (x1 - x3) - y1) % p
+     
+    is_point_on_curve((x3, y3), a, b, p)
+         
+    return x3, y3
 
-def point_doubling(P, a, b, p):
+def is_point_on_curve(P, a, b, p):
+    """
+    Checks if the given point P lies on the elliptic curve y^2 = x^3 + a*x + b mod p.
+
+    Parameters:
+    - P: the point (x, y) to be checked
+    - a, b: coefficients of the elliptic curve equation
+    - p: the prime modulus
+    """
+    x, y = P
+    
+    assert (y*y) % p == ( pow(x, 3, p) + a*x + b ) % p
+
+def double_and_add(G, k, a, b, p):
     """
     Performs point doubling on an elliptic curve.
 
     Parameters:
-    - P: the point (x, y) on the elliptic curve
+    - G: the point (x, y) on the elliptic curve
+    - k: the key to be converted into a binary
     - a, b: coefficients of the elliptic curve equation
     - p: the prime modulus
 
     Returns:
     - The result of the point doubling (x, y)
     """
-    x1, y1 = P
+    target_point = G
     
-    if y1 == 0:
-        return (0, 0)
-    
-    m = ((3 * x1 ** 2 + a) * mod_inverse(2 * y1, p)) % p
-    x3 = (m ** 2 - 2 * x1) % p
-    y3 = (m * (x1 - x3) - y1) % p
-    
-    return (x3, y3)
+    k_binary = bin(k)[2:]
+   
+    for i in range(1, len(k_binary)):
+        current_bit = k_binary[i: i + 1]
+         
+        # doubling - always
+        target_point = point_addition(target_point, target_point, a, b, p)
+         
+        if current_bit == "1":
+            target_point = point_addition(target_point, G, a, b, p)
+     
+    is_point_on_curve(target_point, a, b, p)
+     
+    return target_point
 
-def ecdh(p, a, b, P):
+
+
+def ecdh(G, a, b, p):
     """
     Performs the Elliptic Curve Diffie-Hellman Key Exchange.
 
     Parameters:
+    - B: the point (x, y) on the elliptic curve
     - p: the prime modulus
     - a, b: coefficients of the elliptic curve equation
     - P: the base point (x, y) on the elliptic curve
@@ -129,44 +90,42 @@ def ecdh(p, a, b, P):
     Returns:
     - The shared secret key (x, y) on the elliptic curve
     """
-    # Alice's private and public keys
-    a_private = random.randint(2, p - 2)
-    A_public = double_and_add(P, a_private, a, b, p)
+    # Alice's private key
+    private_key_a = random.getrandbits(256)
     
-    # Bob's private and public keys
-    b_private = random.randint(2, p - 2)
-    B_public = double_and_add(P, b_private, a, b, p)
+    # Bob's private key
+    private_key_b = random.getrandbits(256)
+    
+    # Alice's  public keys
+    public_key_a = double_and_add(G, private_key_a, a, b, p)
+    
+    # Bob's public keys
+    public_key_b = double_and_add(G, private_key_b, a, b, p)
     
     # The secret key computed by Alice
-    T_AB = double_and_add(B_public, a_private,a, b, p)
-    # The secret key computed by Bob
-    T_BA = double_and_add(A_public, b_private, a, b, p)
+    T_AB = double_and_add(public_key_b, private_key_a, a, b, p)
     
-    assert T_AB == T_BA
+    # The secret key computed by Bob
+    T_BA = double_and_add(public_key_a, private_key_b, a, b, p)
+    
+    if T_AB != T_BA:
+        print("Error: Shared secret keys do not match.")
     
     return T_AB
 
 def main():
-    # Choose a prime p and the elliptic curve parameters a, b
-    p = 2**256 - 2**224 + 2**192 + 2**96 - 1
-    a = -3
-    b = 41058363725152142129326129780047268409114441015993725554835256314039467401291
+        # Choose a prime p and the elliptic curve parameters a, b
+    p = pow(2, 256) - pow(2, 32) - pow(2, 9) - pow(2, 8) - pow(2, 7) - pow(2, 6) - pow(2, 4) - pow(2, 0)
+    a = 0
+    b = 7
     
-    # Choose a private element P = (x_p, y_p)
-    x_p = 48439561293906451612451812245956901550880174394880157880096106384 
-    y_p = 36134677641873308953988598641663524294515194642976523804497193542
+    # base point
+    G = (55066263022277343669578718895168534326250603453777594175500187360389116729240, 
+         32670510020758816978083085130507043184471273380659243275938904335757337482424)
     
-    P = (x_p, y_p)
+    # Perform ECDH key exchange
+    joint_secret = ecdh(G, a, b, p)
+    print(f"Shared secret key: {joint_secret}")
     
-    if is_curve_valid(a, b, p):
-        print("The Curve is valid")
-        
-    if is_point_on_curve(P, a, b, p):
-        print(f"Point {P} is on the elliptic curve")
-        
-    # Perform the Elliptic curve Diffie-Hellman Key Exchange
-    joint_secret = ecdh(p, a, b, P)
-    print(f"Joint secret: {joint_secret}")
-
 if __name__ == "__main__":
     main()
